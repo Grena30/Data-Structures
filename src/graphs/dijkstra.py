e5c54cefe754fb8522c89ee3ebcedc5486f11ea0
Dinu Gutu <dinu.guu@autozone.com>
2026-03-26 12:32:31 +0200

    feat: update dijkstra's algorithm

--- src/graphs/dijkstra.py
+++ src/graphs/dijkstra.py
@@ -33,4 +33,38 @@ class DijkstraAlgorithm:
         heapq.heappush(self.heap, start_vertex)
 
         while self.heap:
-            pass
+            
+            actual_vertex = heapq.heappop(self.heap)
+            
+            if actual_vertex.visited:
+                continue
+                
+            for edge in actual_vertex.adjacency_list:
+                u = edge.start_vertex
+                v = edge.target_vertex
+                
+                new_distance = u.min_distance + edge.weight
+                if new_distance < v.min_distance:
+                    v.predecessor = u
+                    v.min_distance = new_distance
+                    
+                    # O(N)
+                    heapq.heappush(self.heap, v)
+            
+            actual_vertex.visited = True
+    
+    
+    @staticmethod
+    def get_shortest_path(vertex):
+        
+        print(f"Shortest path to vertex is: f{str(vertex.min_distance)}")
+        
+        actual_vertex = vertex
+        
+        while actual_vertex is not None:
+            print(actual_vertex.name)
+            actual_vertex = actual_vertex.predecessor
+            
+            
+if __name__ == "__main__":
+    pass
